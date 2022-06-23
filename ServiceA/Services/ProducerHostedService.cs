61560064e4f7d1c8d6a9cdad69b07bcf5ad05394
Sean <sean@skrmiish.com>
2022-06-23 14:42:32 +0200

    Everything working just need to clean up

--- ServiceA/Services/ProducerHostedService.cs
+++ ServiceA/Services/ProducerHostedService.cs
@@ -1,13 +1,54 @@
+using Common.Models;
+using Common.RabbitMqComponents;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace ServiceA.Services;
 
 public class ProducerHostedService : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    
+    private readonly RabbitMqProducerBase<RabbitMqMessage<PayloadBase>> _producer;
+    private readonly ILogger _logger;
+
+    public ProducerHostedService(
+        RabbitMqProducerBase<RabbitMqMessage<PayloadBase>> producer,
+        ILogger logger
+    )
     {
-        // TODO: Implement ConsoleListener
+        _producer = producer;
+        _logger = logger;
+    } 
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Console.WriteLine("Please enter your name: ");
+            var name = Console.ReadLine();
+            var message = $"Hello my name is, {name}";
+            PublishMessage(message, name);
+            Console.WriteLine($"Hello {name}!");
+        }
+
+        await Task.CompletedTask;
+    }
+
+    
+
+    private void PublishMessage(string? message, string? name)
+    {
+        var payload = new PayloadBase
+        {
+            Message = message,
+            Name = name
+        };
+
+        var rabbitMqPayload = new RabbitMqMessage<PayloadBase>
+        {
+            Payload = payload,
+        };
         
-        return Task.CompletedTask;
+        _producer.Publish(rabbitMqPayload);
     }
 }
