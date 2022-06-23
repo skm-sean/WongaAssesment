2e4b4e8e26cdfd59b07522f691640c808b3d5a0e
Sean <sean@skrmiish.com>
2022-06-23 13:34:38 +0200

    Added rabbitMqBase for connecting to rabbitMQ instance

---
+++ Common/RabbitMqComponents/RabbitMqBase.cs
@@ -0,0 +1,65 @@
+using RabbitMQ.Client;
+using Serilog;
+
+namespace Common.RabbitMqComponents;
+
+public class RabbitMqBase : IDisposable
+{
+    private readonly ILogger _logger;
+    private readonly ConnectionFactory _connectionFactory;
+
+    private IModel? Channel { get; set; }
+    private IConnection? _connection;
+    
+    protected RabbitMqBase(
+        ILogger logger,
+        ConnectionFactory connectionFactory)
+    {
+        _logger = logger;
+        _connectionFactory = connectionFactory;
+        ConnectToRabbitMq();
+    }
+
+    private void ConnectToRabbitMq()
+    {
+        if (_connection is not {IsOpen: true})
+        {
+            _connection = _connectionFactory.CreateConnection();
+        }
+        
+        _connection.ConnectionShutdown += ConnectionOnConnectionShutdown;
+
+        if (Channel is {IsOpen: true}) 
+            return;
+        
+        Channel = _connection.CreateModel();
+        Channel.QueueDeclare(
+            queue: "rabbitQue",
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+    }
+
+    private void ConnectionOnConnectionShutdown(object? sender, ShutdownEventArgs e)
+    {
+        
+    }
+    
+    public void Dispose()
+    {
+        try
+        {
+            Channel?.Close();
+            Channel?.Dispose();
+            Channel = null;
+            _connection?.Close();
+            _connection?.Dispose();
+            _connection = null;
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Unable to dispose of {Class}", nameof(RabbitMqBase));
+        }
+    }
+}
---
+++ Common/RabbitMqComponents/RabbitMqConsumerBase.cs
@@ -0,0 +1,6 @@
+namespace Common.RabbitMqComponents;
+
+public class RabbitMqConsumerBase
+{
+    
+}
